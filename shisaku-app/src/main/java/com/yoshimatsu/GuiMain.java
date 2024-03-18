8680c4376125da9ea426b36abca89990cfeb2e36
ayoshimatsu <yoshimaster7@gmail.com>
2024-03-18 13:32:54 +0900

    Delete the first directories

--- shisaku-app/src/main/java/com/yoshimatsu/GuiMain.java
+++
@@ -1,8 +0,0 @@
-package com.yoshimatsu;
-
-public class GuiMain {
-    public static void main( String[] args ) {
-        
-    }
-
-}
--- shisaku-app/src/main/java/com/yoshimatsu/ServerSocketThread.java
+++
@@ -1,8 +0,0 @@
-package com.yoshimatsu;
-
-public class ServerSocketThread extends Thread {
-    @Override
-    public void run() {
-        System.out.println("ServerThreadStart");
-    }
-}

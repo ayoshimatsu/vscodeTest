cdfe11cca904b285fd535a4f41c6eb164035e2fb
ayoshimatsu <yoshimaster7@gmail.com>
2024-03-19 23:41:54 +0900

    can buil, but can not execute jar

--- shisaku-app/component/src/main/java/com/component/ButtonAction.java
+++ shisaku-app/component/src/main/java/com/component/ButtonAction.java
@@ -1,8 +1,9 @@
-package com.yoshimatsu.communicate;
+package com.component;
 
 import java.awt.event.*;
+import javax.swing.JButton;
 
-public class ButtonAction implements ActionListener {
+public class ButtonAction extends JButton implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent aEvent) {
         System.out.println("button is pressed.");
--- shisaku-app/development/src/main/java/com/yoshimatsu/MainFrame.java
+++ shisaku-app/development/src/main/java/com/yoshimatsu/MainFrame.java
@@ -1,10 +1,20 @@
 package com.yoshimatsu;
 
-import java.awt.Container;
-
+import java.awt.BorderLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
-public class MainFrame extends JFrame {
+import com.component.*;
+
+public class MainFrame extends JFrame{
+
+    public MainFrame() {
+        JButton button1 = new JButton("test");  
+        button1.addActionListener(new ButtonAction());
+
+        getContentPane().add(BorderLayout.WEST, button1);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(200, 100);
+    }
 
 }
